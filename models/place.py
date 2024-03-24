eb991e0b50a35632732c5ca9e41048d60191cdfa
Justxd22 <xdjust18@gmail.com>
2024-03-24 04:40:15 +0200

    ffff

--- models/place.py
+++ models/place.py
@@ -2,11 +2,20 @@
 """Place Module for HBNB project."""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 import models
 import os
 import shlex
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
 class Place(BaseModel, Base):
     """A place to stay."""
