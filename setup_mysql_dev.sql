590f4566bc5a06dc93766e976a8db23da7dd94d7
Justxd22 <xdjust18@gmail.com>
2024-03-24 02:39:03 +0200

    new db(s)

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- make new sql db for airbnb if it doesn't exsit
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO  'hbnb_dev'@'localhost';
