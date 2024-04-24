375b832a36378d3908d2b8250ea70c9fe5532eb7
Justxd22 <xdjust18@gmail.com>
2024-04-24 23:20:53 +0200

    e

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def list():
     """HEllo."""
-    return render_template('7-states_list.html', x="States",
+    return render_template('7-states_list.html',
                            d=storage.all('State'))
 
 
